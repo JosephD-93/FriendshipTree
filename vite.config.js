c0f84bedc64691e65514796cf5bfef597be70e7d
JosephD-93 <josephdraper93@gmail.com>
2026-08-28 19:59:12 +0100

    Apply native touch handling to daily planner build

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { dailyPlannerTouchTransform } from './build/dailyPlannerTouchTransform.js'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [dailyPlannerTouchTransform(), react()],
   base: './',
   build: {
     outDir: 'dist',
