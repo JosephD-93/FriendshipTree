23b9992e18e85f9cc8a29b33d2d455d2db9f0d88
JosephD-93 <josephdraper93@gmail.com>
2026-08-28 22:01:59 +0100

    Keep FriendshipTree WebView renderer alive on app resume

--- android/app/src/main/java/io/github/josephd93/friendshiptree/MainActivity.java
+++ android/app/src/main/java/io/github/josephd93/friendshiptree/MainActivity.java
@@ -8,24 +8,66 @@ import com.getcapacitor.Plugin;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
 // ModifiedMainActivityForSocialLoginPlugin is VERY VERY important !!!!!!
 public class MainActivity extends BridgeActivity implements ModifiedMainActivityForSocialLoginPlugin {
+    private static final String LIFECYCLE_TAG = "FT-Lifecycle";
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        Log.i(LIFECYCLE_TAG, "onCreate savedState=" + (savedInstanceState != null));
         registerPlugin(GoogleDriveAuthorizationPlugin.class);
         super.onCreate(savedInstanceState);
+
+        // FriendshipTree can hold many decoded profile/gallery images. Android may otherwise
+        // reclaim the WebView renderer as soon as the activity is backgrounded, which makes
+        // returning to the app look like a full page refresh and forces the photos to decode
+        // again. Keep the renderer bound to this foreground activity and do not waive its
+        // priority merely because the WebView is temporarily not visible.
+        WebView webView = getBridge() != null ? getBridge().getWebView() : null;
+        if (webView != null) {
+            webView.setRendererPriorityPolicy(WebView.RENDERER_PRIORITY_BOUND, false);
+            Log.i(LIFECYCLE_TAG, "WebView renderer priority policy set to BOUND / waiveWhenNotVisible=false");
+        } else {
+            Log.w(LIFECYCLE_TAG, "WebView unavailable while applying renderer priority policy");
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.i(LIFECYCLE_TAG, "onStart");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.i(LIFECYCLE_TAG, "onResume");
     }
 
     @Override
     public void onPause() {
+        Log.i(LIFECYCLE_TAG, "onPause");
         super.onPause();
         // Refresh the home-screen tracker after in-app changes have been
         // mirrored to Capacitor Preferences.
         Intent refreshWidget = new Intent(this, HealthWidgetProvider.class);
         refreshWidget.setAction(HealthWidgetProvider.ACTION_REFRESH);
         sendBroadcast(refreshWidget);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.i(LIFECYCLE_TAG, "onStop");
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.i(LIFECYCLE_TAG, "onDestroy changingConfigurations=" + isChangingConfigurations());
+        super.onDestroy();
     }
 
     @Override
